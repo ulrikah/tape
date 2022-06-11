cf6d570fa21a671b77e24c3faee37bf2f8e3ece3
Ulrik Antoniussen Halmøy <ulrikhalmoy@gmail.com>
2022-06-11 15:05:06 +0200

    Script to make new Tidal file

---
+++ tidal/file.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+LC_ALL=no_NO.UTF-8
+timestamp=$(date -u +%Y%m%d_%H%M%S)
+
+
+filename="yes_${timestamp}.tidal"
+echo "Creating a new Tidal file called $filename"
+touch $filename 
