49e7522fbe8bfe00170b109b4256879aa2016302
ulrikah <ulrikhalmoy@gmail.com>
2022-11-28 18:26:09 +0100

    Fix: new scores files are placed under scores/

--- tidal/file.sh
+++ tidal/file.sh
@@ -7,6 +7,7 @@ timestamp=$(date -u +%Y%m%d_%H%M%S)
 
 filename="untitled_${timestamp}.tidal"
 echo "Creating a new Tidal file called $filename"
-touch $filename 
+output_file="scores/$filename"
+touch $output_file
 
-echo "setcps (140/60/4)" >> $filename
+echo "setcps (140/60/4)" >> $output_file
