a8551c5738deb6c355ce5336b187f9383ee91c4f
Ulrik Antoniussen Halmøy <ulrikhalmoy@gmail.com>
2025-02-23 21:03:01 +0100

    add optional filename for file creation

--- tidal/file.sh
+++ tidal/file.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
 
+if [[ $1 ]]; then
+    score_name=$1
+else
+    score_name="untitled"
+fi
+
 LC_ALL=no_NO.UTF-8
 timestamp=$(date -u +%Y%m%d_%H%M%S)
-
-
-filename="untitled_${timestamp}.tidal"
+filename="${timestamp}_${score_name}.tidal"
 output_file="scores/$filename"
-echo "Creating a new Tidal file called $output_file"
+echo "Creating a new Tidal score: $output_file"
 touch $output_file
 
 echo "setcps (140/60/4)" >> $output_file
